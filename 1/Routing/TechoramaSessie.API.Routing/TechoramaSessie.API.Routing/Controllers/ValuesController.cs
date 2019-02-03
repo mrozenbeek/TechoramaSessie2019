0ec472be87b6855af4adb4b7f6d02e2baffe8f6c
Michael Rozenbeek <michaelrozenbeek@hotmail.com>
2019-02-03 19:19:10 +0100

    Added basic controller with code to show default routing.

--- 1/Routing/TechoramaSessie.API.Routing/TechoramaSessie.API.Routing/Controllers/ValuesController.cs
+++ 1/Routing/TechoramaSessie.API.Routing/TechoramaSessie.API.Routing/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechoramaSessie.API.Routing.Models;
 
 namespace TechoramaSessie.API.Routing.Controllers
 {
@@ -10,24 +11,39 @@ namespace TechoramaSessie.API.Routing.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private static IList<string> _data = new List<string>() {
+             "value1", "value2"
+        };
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IList<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_data);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return Ok( _data[id]);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<string> Post([FromBody] ExampleViewModel model)
         {
+            //if (string.IsNullOrWhiteSpace(model.Value))
+            //    return BadRequest();
+
+            if (ModelState.IsValid == false)
+                return BadRequest();
+
+            string manipulatedString = $"added '{model.Value}' by using the post ";
+
+            _data.Add(manipulatedString);
+
+            return Ok(manipulatedString);
         }
 
         // PUT api/values/5
@@ -38,8 +54,15 @@ namespace TechoramaSessie.API.Routing.Controllers
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            if (id < 0)
+                return BadRequest();
+
+            //Will fail if id exceeds the length of the list....
+            _data.RemoveAt(id);
+
+            return Ok();
         }
     }
 }
---
+++ 1/Routing/TechoramaSessie.API.Routing/TechoramaSessie.API.Routing/Models/ExampleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechoramaSessie.API.Routing.Models
+{
+    public class ExampleViewModel
+    {
+        [Required]
+        public string Value { get; set; }
+    }
+}
