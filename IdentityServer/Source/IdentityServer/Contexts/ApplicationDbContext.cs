ccc1e607c3b0edae825e006c6a7a9a0306637f34
Ronald van Meer <ronaldvanmeer@gmail.com>
2019-02-17 20:41:59 +0100

    Initial create

---
+++ IdentityServer/Source/IdentityServer/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// On Model Creating override
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
